52c07be43658bd3314bac3006a933c2a9a7cfbd1
Kenny Henbest <lekenbest@gmail.com>
2019-02-08 16:58:15 -0700

    letstrythisagain

--- game2.js
+++ game2.js
@@ -1,15 +1,3 @@
-// 1. Add 2 more buttons Punch, and Kick to the UI, as well as their respective functions in javascript.
-//   - Have the punch function decrease the target's health by 5, and kick by 10.
-//     - Don't forget to call update inside each function.
-// 2. Declare 2 more variables 'name', and 'hits' where you initialized the health variable. 
-//   - Name your target whatever you want, what datatype would a name be ?
-//     -Set the variable hits with a value of 0, every time the target is hit by a Slap, Punch, or Kick
-// this variable should be increased by 1.
-// 3. Add a placeholder for target name, and hits inside the header next to health.
-// 4. Wire everything up like you did for "Slap".
-
-
-
 let Defenders = {
     Ryu: {
         name: 'Ryu',
