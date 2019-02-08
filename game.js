0e7f404c7bbd4d7d28e4c5a843267cf027c271a5
Kenny Henbest <lekenbest@gmail.com>
2019-02-08 14:42:41 -0700

    with-dictionary

--- game.js
+++ game.js
@@ -1,24 +1,62 @@
-// Create a global variable: var health = 100;
-// Create a function: slap()
-// Have the function reduce the health variable by 1.
-// For now, have the function alert(health).
-// Test the function by calling slap() at the end of the game.js file.
-// You should see an alert of 99 show on the screen.
-// If this is working, remove the test to prevent popups on every page load.
+// 1. Add 2 more buttons Punch, and Kick to the UI, as well as their respective functions in javascript.
+//   - Have the punch function decrease the target's health by 5, and kick by 10.
+//     - Don't forget to call update inside each function.
+// 2. Declare 2 more variables 'name', and 'hits' where you initialized the health variable. 
+//   - Name your target whatever you want, what datatype would a name be ?
+//     -Set the variable hits with a value of 0, every time the target is hit by a Slap, Punch, or Kick
+// this variable should be increased by 1.
+// 3. Add a placeholder for target name, and hits inside the header next to health.
+// 4. Wire everything up like you did for "Slap".
 
 
-var health = 100;
+
+let Defenders = {
+    Ryu: {
+        name: 'Ryu',
+        dHealth: 120,
+    },
+    Ken: {
+        name: 'Ken',
+        dHealth: 130,
+    },
+    George: {
+        name: 'George',
+        dHealth: 110,
+    },
+    Jefferson: {
+        name: 'Jefferson',
+        dHealth: 100,
+    },
+    Batman: {
+        name: 'Batman',
+        dHealth: 220,
+    }
+}
+let health = Defenders.Batman.dHealth;
+let name = Defenders.Batman.name;
+let hits = 0;
+
 
 function slap() {
     health -= 1;
-    console.log(health);
-
+    console.log();
+    update();
 }
-
+function punch() {
+    health -= 5;
+    console.log(health);
+    update();
+}
+function kick() {
+    health -= 10;
+    console.log(health);
+    update();
+}
 
 
 function update() {
-    document.getElementById('health').innerText = health;
-    slap();
+    document.getElementById('health').innerText = Defenders.Batman.dHealth
+    document.getElementById('target').innerText = Defenders.Batman.name;
 }
+
 update()
